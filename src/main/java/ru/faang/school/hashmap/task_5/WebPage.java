e1303cbbe2215887f92d91d6546e31a126ef8f25
*Yuliya <yuliya.klimovich@icloud.com>
2023-06-17 09:11:54 +0200

    refactoring

--- src/main/java/ru/faang/school/hashmap/task_5/WebPage.java
+++ src/main/java/ru/faang/school/hashmap/task_5/WebPage.java
@@ -3,6 +3,8 @@ package ru.faang.school.hashmap.task_5;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
+import java.util.Objects;
+
 @Getter
 @AllArgsConstructor
 public class WebPage {
@@ -10,4 +12,16 @@ public class WebPage {
     private String title;
     private String content;
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        WebPage webPage = (WebPage) o;
+        return url.equals(webPage.url) && title.equals(webPage.title) && content.equals(webPage.content);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(url, title, content);
+    }
 }
